3a86d473979a0de3e6fdcbfc04e2c2d8d0beef67
AbdillahHamzahAli <hamzahjmbg14@gmail.com>
2023-12-17 21:48:38 +0700

    auth signIn and signUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "./lib/helper/supabaseClient";
-import Books from "./components/Books";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Books from "./pages/Books";
 
 export default function App() {
   const [user, setUser] = useState(null);
   const [session, setSession] = useState(null);
-  const login = async () => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-    if (error) throw error;
-  };
-  const logout = async () => {
-    await supabase.auth.signOut();
-  };
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -34,11 +28,16 @@ export default function App() {
         default:
           break;
       }
-      setSession(session);
     });
-
     return () => subscription.unsubscribe();
   }, []);
 
-  return <div>{user ? <Books user={user} /> : <button onClick={login}>Login with github</button>}</div>;
+  // return <div>{user ? <Books user={user} /> : <button onClick={login}>Login with github</button>}</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      {user ? <Route path="/books" element={<Books user={user} />} /> : ""}
+    </Routes>
+  );
 }
