6210fcb04725044130e25068d6df38293685bf59
AbdillahHamzahAli <hamzahjmbg14@gmail.com>
2023-12-21 12:32:03 +0700

    Read Books

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -3,51 +3,23 @@ import { supabase } from "../../lib/helper/supabaseClient";
 import { Link } from "react-router-dom";
 
 export default function Books({ user }) {
-  const [error, setError] = useState(null);
-  const [books, setBooks] = useState([]);
-  const [formData, setFormData] = useState({
-    title: "",
-    thumbnail: "",
-    many_pages: "",
-    page_read: "",
-  });
+  const CDNURL = "https://pnjolpsznjbefwhxrhds.supabase.co/storage/v1/object/public/book-cover/";
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
+  const [data, setData] = useState([]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // Function
+  const getData = async () => {
     try {
-      const { data } = await supabase
-        .from("Books")
-        .upsert([{ ...formData, user_id: user.id }])
-        .select("*");
-      setFormData({
-        title: "",
-        thumbnail: "",
-        many_pages: "",
-        page_read: "",
-      });
+      const { data } = await supabase.from("Books").select("*").eq("user_id", user.id);
+      setData(data);
     } catch (error) {
-      setError(error);
+      console.error("Error fetching data:", error);
     }
   };
 
-  const logout = async () => {
-    await supabase.auth.signOut();
-  };
-
-  const getBooks = async () => {
-    let { data: Books, error } = await supabase.from("Books").select("*");
-    setBooks(Books);
-  };
   useEffect(() => {
-    getBooks();
-
-    return () => {};
-  }, [books, formData]);
+    getData();
+  }, [user.id]); // Bergantung pada perubahan user.id
 
   return (
     <div>
@@ -72,27 +44,17 @@ export default function Books({ user }) {
       <div className="w-full px-12 bg-gray-100">
         <div className="flex items-center justify-center py-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div className="relative h-full rounded-lg shadow-md overflow-hidden">
-              <img src="https://c4.wallpaperflare.com/wallpaper/512/813/273/anime-anime-girls-hd-wallpaper-preview.jpg" alt="Gambar Card" className="w-96 h-96 object-cover" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-              <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
-                <h2 className="text-2xl font-semibold ">Shenhe Genshin</h2>
-              </div>
-            </div>
-            <div className="relative h-full rounded-lg shadow-md overflow-hidden">
-              <img src="https://c4.wallpaperflare.com/wallpaper/549/839/634/anime-girls-anime-edit-hd-wallpaper-preview.jpg" alt="Gambar Card" className="w-96 h-96 object-cover" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-              <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
-                <h2 className="text-2xl font-semibold ">Anime Girl</h2>
-              </div>
-            </div>
-            <div className="relative h-full rounded-lg shadow-md overflow-hidden">
-              <img src="https://c4.wallpaperflare.com/wallpaper/488/946/398/anime-anime-girls-oshi-no-ko-kurokawa-akane-hd-wallpaper-preview.jpg" alt="Gambar Card" className="w-96 h-96 object-cover" />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
-              <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
-                <h2 className="text-2xl font-semibold ">Oshi No Kho</h2>
-              </div>
-            </div>
+            {data.map((item, i) => {
+              return (
+                <div key={i} className="relative h-full rounded-lg shadow-md overflow-hidden">
+                  <img src={CDNURL + user.id + "/" + item.thumbnail} alt="Gambar Card" className="w-96 h-96 object-cover" />
+                  <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
+                  <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
+                    <h2 className="text-2xl font-semibold ">{item.title}</h2>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
